ef3b2ea5662d0d1f533efe999ff1538edb8ce7c5
GabrielMello2025 <gabrielmello0255@gmail.com>
2025-09-07 16:28:51 -0300

    Adicionado programação orientada objeto, e arraylist

--- Gastos.java
+++ Gastos.java
@@ -2,13 +2,13 @@ public class Gastos {
     private String nomeGasto;
     private double gastosMensal;
 
-    // 🔹 Construtor correto
+    
     public Gastos(String nomeGasto , double gastosMensal) {
         this.nomeGasto = nomeGasto;
         this.gastosMensal = gastosMensal;
     }
 
-    // Getters
+    
     public String getNomeGasto() {
         return nomeGasto;
     }
